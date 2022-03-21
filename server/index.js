85ae46ca497459e75546e9481517a2d2d9a47055
Katlego <kmokoena@student.wethinkcode.co.za>
2022-03-21 15:49:45 +0200

    established first database connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
-const express = require('express');
+const express = require("express");
+var sqlite3 = require("sqlite3").verbose();
+
 const app = express();
+const port = process.env.port || 5000;
 
-const port = process.env.port || 5000
+var db = new sqlite3.Database("DB");
 
-app.route("/").get((req, res) => res.json("Hello World!"));
+//connect to database
+db.serialize(function () {
+    // create user table
+    db.run("CREATE TABLE user (id INT, username TEXT)");
+    var stmt = db.prepare("INSERT INTO user VALUES (?,?)");
+    for (var i = 0; i < 10; i++) {
+        var date = new Date();
+        var d = date.toLocaleDateString();
+        stmt.run(i, d);
+    }
+    // add data to database
+    stmt.finalize();
+
+    // select all users
+    db.each("SELECT id, username FROM user", function (err, row) {
+        console.log("User id : " + row.id, row.username);
+    });
+});
+
+db.close();
 
 app.listen(port, () => console.log(`listening on port ${port}`));
