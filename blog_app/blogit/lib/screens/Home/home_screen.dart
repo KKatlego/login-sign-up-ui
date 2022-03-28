fdc64a403017287b6a3dc37d9741c176929e0cfc
Katlego <kmokoena@student.wethinkcode.co.za>
2022-03-28 20:08:03 +0200

    home screen layout

--- blog_app/blogit/lib/screens/Home/home_screen.dart
+++ blog_app/blogit/lib/screens/Home/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-
 import 'login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,8 +6,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Contents(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          children: const [
+            SizedBox(height: 10.0),
+            Text(
+              "Welcome to Blog It",
+              style: TextStyle(color: Colors.black),
+            ),
+            SizedBox(height: 3.0),
+            Text("Nice to see you, let's get blogging.",
+                style: TextStyle(color: Colors.grey, fontSize: 14.0))
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: const Contents(),
     );
   }
 }
@@ -21,52 +35,64 @@ class Contents extends StatelessWidget {
   Widget build(BuildContext context) {
     // total height and width of screen
     Size size = MediaQuery.of(context).size;
-    const String assetName = 'assets/icons/Social_Media_Flatline.svg';
-
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            "Welcome to BlogIt",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          SizedBox(height: size.height * 0.05),
-          SvgPicture.asset(assetName, semanticsLabel: 'Social media page'),
-          SizedBox(height: size.height * 0.05),
-          SizedBox(
-              width: size.width * 0.8,
-              child:
-                  // login button
-                  ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color.fromRGBO(104, 225, 253, 1)),
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.black),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+                width: size.width * 0.8,
+                height: size.height * 0.06,
+                child:
+                    // login button
+                    ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color.fromRGBO(104, 225, 253, 1)),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
                           ),
                         ),
-                      ),
-                      child: const Text('LOGIN',
-                          style: TextStyle(
-                              fontSize: 14, fontWeight: FontWeight.bold)),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => (const LoginScreen())));
-                      })),
-          // sign up button
-          TextButton(
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-              ),
-              child: const Text("Dont have an account? Sign up",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-              onPressed: () {}),
-        ]);
+                        child: const Text('LOGIN',
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold)),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => (const LoginScreen())));
+                        })),
+            SizedBox(height: size.height * 0.01),
+
+            // sign up button
+            SizedBox(
+                width: size.width * 0.8,
+                height: size.height * 0.06,
+                child:
+                    // login button
+                    ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color.fromARGB(221, 17, 17, 17)),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                          ),
+                        ),
+                        child: const Text('SIGN UP',
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold)),
+                        onPressed: () {})),
+          ]),
+    );
   }
 }
