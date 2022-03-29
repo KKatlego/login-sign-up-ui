2443d9322d108e61d3957643928d5463497f0a6e
Katlego <kmokoena@student.wethinkcode.co.za>
2022-03-29 18:05:00 +0200

    home screen | commented server code

--- blog_app/blogit/lib/screens/home_screen.dart
+++ blog_app/blogit/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:http/http.dart';
 import 'welcome_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -20,49 +21,88 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class Contents extends StatelessWidget {
+class Contents extends StatefulWidget {
   const Contents({Key? key}) : super(key: key);
 
+  @override
+  State<Contents> createState() => _ContentsState();
+}
+
+class _ContentsState extends State<Contents> {
+    // String serverResponse = 'Server response';
   @override
   Widget build(BuildContext context) {
     // total height and width of screen
     Size size = MediaQuery.of(context).size;
     return Container(
       alignment: Alignment.center,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(child: Text('blogs')),
-            SizedBox(height: size.height * 0.02),
-            SizedBox(
-                width: size.width * 0.8,
-                height: size.height * 0.06,
-                child:
-                    // login button
-                    ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              const Color.fromARGB(221, 17, 17, 17)),
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                            ),
-                          ),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+          Widget>[
+        const SizedBox(child: Text('blogs')),
+        SizedBox(height: size.height * 0.02),
+        SizedBox(
+            width: size.width * 0.8,
+            height: size.height * 0.06,
+            child:
+                // login button
+                ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(221, 172, 172, 172)),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
                         ),
-                        child: const Text('LOGOUT',
-                            style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.bold)),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      (const WelcomeScreen())));
-                        })),
-          ]),
+                      ),
+                    ),
+                    child: const Text('Click here to connect to server',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold)),
+                    onPressed: () {
+// _makeGetRequest;
+                    })),
+        SizedBox(
+            width: size.width * 0.8,
+            height: size.height * 0.06,
+            child:
+                // login button
+                ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(221, 17, 17, 17)),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                    ),
+                    child: const Text('LOGOUT',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold)),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => (const WelcomeScreen())));
+                    })),
+      ]),
     );
   }
+  //   _makeGetRequest() async {
+  //   final url = Uri.parse(_localhost());
+  //   Response response = await get(url);
+  //   setState(() {
+  //     serverResponse = response.body;
+  //   });
+  // }
+
+  // String _localhost() {
+  //     return 'http://localhost:5000';
+  // }
 }
+
+
--- server/index.js
+++ server/index.js
@@ -8,31 +8,31 @@ const port = process.env.port || 5000;
 app.route("/").get((req, res) =>res.json("fist rest api"))
 
 //connect to database
-const db = new sqlite3.Database("./sample.db", sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        return console.error(err.message);
-    } else {
-        console.log("Conected successfully to the database");
-    }
-});
+// const db = new sqlite3.Database("./sample.db", sqlite3.OPEN_READWRITE, (err) => {
+//     if (err) {
+//         return console.error(err.message);
+//     } else {
+//         console.log("Conected successfully to the database");
+//     }
+// });
 
-// select all rows
-const sql = `SELECT * FROM users`
-
-
-db.all(sql, [], (err, rows) => {
-    if (err) {
-        return console.error(err.message);
-    } else{
-        rows.forEach(row => console.log(row));
-    }
-});
+// // select all rows
+// const sql = `SELECT * FROM users`
 
 
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-});
+// db.all(sql, [], (err, rows) => {
+//     if (err) {
+//         return console.error(err.message);
+//     } else{
+//         rows.forEach(row => console.log(row));
+//     }
+// });
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+
+// db.close((err) => {
+//     if (err) {
+//         return console.error(err.message);
+//     }
+// });
+
+app.listen(port, () => console.log(`listening on port localhost: ${port}`));
